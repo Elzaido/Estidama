027e44fa7c5191d0e6a36f82b3d63d996b9ed888
Elzaido <91469219+Elzaido@users.noreply.github.com>
2023-09-17 12:47:17 +0300

    تكاتيك

--- lib/ui/pages/main_pages/home.dart
+++ lib/ui/pages/main_pages/home.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:madenati/constants/carsousel_items.dart';
 import 'package:madenati/ui/widgets/carousel.dart';
 import '../../../models/gridmodel.dart';
 import '../../widgets/interface_components.dart';
 import '../../widgets/grid_widget.dart';
-import '../../widgets/interface_components.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {List<Widget> images = [
-  carsoulItem(
-      image: 'assets/volunteering.png',
-      title: 'تطوع',
-      text:
-          'قام أحد فرق التطوع في مدينة إربد بزراعة 20 شجرة على طريق الواصل بين منطقة أيدون و الحصن'),
-]; 
+  Widget build(BuildContext context) {
+    List<Widget> images = [
+      carsoulItem(
+          image: 'assets/volunteering.png',
+          title: 'تطوع',
+          text:
+              'قام أحد فرق التطوع في مدينة إربد بزراعة 20 شجرة على طريق الواصل بين منطقة أيدون و الحصن'),
+    ];
     return Scaffold(
         appBar:
             defaultAppBar(context: context, title: 'الرئيسية', isHome: true),
